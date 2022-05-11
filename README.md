0d185c95bff146b6da669c8efbc0a1e6c5235bf7
Jean Lee <jeanlee.jingli@icloud.com>
2022-05-11 16:26:26 -0700

    modify READEME.md, index.html, script.js and style.css

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 
 ## Description
 
+This is a weather dashboard with form inputs, you can get the current and the future weather conditions for that city by clicking the searching a city button.
 
 ## How to use the application
 
+WHEN you search for a city, the weather dashboard show you the current and future wether of this city;
+WHEN you view current weather conditions for a city, it shows you the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index;
+WHEN you view the UV index, it shows you a color that indicates whether the conditions are favorable, moderate, or severe;
+WHEN you view future weather conditions for a city, it shows you a 5-day forecast that displays the date, an icon with weather condition descriptions , the temperature, the wind speed, and the humidity;
+WHEN you click on a city in the search history, it shows you current and future conditions for that city again;
 
 ## Live Link
 https://jean424.github.io/Weather_Dashboard_App/
--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function currentWeather(city){
       // Display the current weather 
       console.log(response);
   
-      var weathericon= response.weather[0].icon;
+      var weathericon = response.weather[0].icon;
       var iconURL="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
 
       var date=new Date(response.dt*1000).toLocaleDateString();
@@ -108,13 +108,14 @@ function forecast(cityID){
       for (i=0;i<5;i++){
           var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
           var iconcode= response.list[((i+1)*8)-1].weather[0].icon;
+          var weatherdescrition = response.list[((i+1)*8)-1].weather[0].description;
           var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
           var tempK= response.list[((i+1)*8)-1].main.temp;
           var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
           var humidity= response.list[((i+1)*8)-1].main.humidity;
       
           $("#fDate"+i).html(date);
-          $("#fImg"+i).html("<img src="+iconurl+">");
+          $("#fImg"+i).html("<img src="+iconurl+">"+weatherdescrition);
           $("#fTemp"+i).html(tempF+"&#8457");
           $("#fHumidity"+i).html(humidity+"%");
       }
